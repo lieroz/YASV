cc4ccc2c4d8bcb0ddbddda988044016a89ce41e6
Andrey Kamakin <a.kamakin@icloud.com>
2024-04-19 16:54:25 +0300

    add physical device selection

--- RHI/VulkanDevice.cs
+++ RHI/VulkanDevice.cs
@@ -25,18 +25,25 @@ internal sealed class VulkanException(string? message = null) : Exception(messag
 public class VulkanDevice : RenderingDevice
 {
 #if DEBUG
-    private static readonly string[] ValidationLayers = [
+    private static readonly string[] _validationLayers = [
         "VK_LAYER_KHRONOS_profiles",
         "VK_LAYER_KHRONOS_validation",
     ];
 #endif
 
-    private readonly Vk vk = Vk.GetApi();
-    private Instance instance;
+    private readonly Vk _vk = Vk.GetApi();
+    private Instance _instance;
 #if DEBUG
-    private ExtDebugUtils? extDebugUtils;
-    private DebugUtilsMessengerEXT debugMessenger;
+    private ExtDebugUtils? _extDebugUtils;
+    private DebugUtilsMessengerEXT _debugMessenger;
 #endif
+    private PhysicalDevice _physicalDevice;
+    private struct QueueFamilyIndices
+    {
+        public int? Graphics { get; set; }
+
+        public readonly bool IsComplete() => Graphics.HasValue;
+    }
 
     public override unsafe void Create(Sdl sdlApi)
     {
@@ -44,12 +51,13 @@ public class VulkanDevice : RenderingDevice
 #if DEBUG
         SetupDebugMessenger();
 #endif
+        PickPhysicalDevice();
     }
 
     public override unsafe void Destroy()
     {
 #if DEBUG
-        extDebugUtils?.DestroyDebugUtilsMessenger(instance, debugMessenger, null);
+        _extDebugUtils?.DestroyDebugUtilsMessenger(_instance, _debugMessenger, null);
 #endif
         DestroyInstance();
     }
@@ -85,14 +93,14 @@ public class VulkanDevice : RenderingDevice
         };
 
 #if DEBUG
-        instanceCreateInfo.EnabledLayerCount = (uint)ValidationLayers.Length;
-        instanceCreateInfo.PpEnabledLayerNames = (byte**)SilkMarshal.StringArrayToPtr(ValidationLayers);
+        instanceCreateInfo.EnabledLayerCount = (uint)_validationLayers.Length;
+        instanceCreateInfo.PpEnabledLayerNames = (byte**)SilkMarshal.StringArrayToPtr(_validationLayers);
 
         var debugMessengerCreateInfo = GetDebugMessengerCreateInfo();
         instanceCreateInfo.PNext = &debugMessengerCreateInfo;
 #endif
 
-        var result = vk.CreateInstance(instanceCreateInfo, null, out instance);
+        var result = _vk.CreateInstance(instanceCreateInfo, null, out _instance);
 
         SilkMarshal.Free((nint)applicationName);
         SilkMarshal.Free((nint)engineName);
@@ -109,18 +117,18 @@ public class VulkanDevice : RenderingDevice
     {
         uint layerCount = 0;
 
-        var result = vk.EnumerateInstanceLayerProperties(&layerCount, null);
+        var result = _vk.EnumerateInstanceLayerProperties(&layerCount, null);
         VulkanException.ThrowsIf(result != Result.Success, $"Couldn't determine instance layer properties count: {result}.");
 
         var availableLayersProperties = new LayerProperties[layerCount];
         fixed (LayerProperties* layerPropertiesPtr = availableLayersProperties)
         {
-            result = vk.EnumerateInstanceLayerProperties(&layerCount, availableLayersProperties);
+            result = _vk.EnumerateInstanceLayerProperties(&layerCount, availableLayersProperties);
             VulkanException.ThrowsIf(result != Result.Success, $"Couldn't enumerate instance layer properties: {result}.");
         }
 
         var availableLayersNames = availableLayersProperties.Select(layer => SilkMarshal.PtrToString((nint)layer.LayerName)).ToHashSet();
-        return ValidationLayers.All(availableLayersNames.Contains);
+        return _validationLayers.All(availableLayersNames.Contains);
     }
 #endif
 
@@ -169,12 +177,91 @@ public class VulkanDevice : RenderingDevice
 
     private unsafe void SetupDebugMessenger()
     {
-        VulkanException.ThrowsIf(!vk.TryGetInstanceExtension(instance, out extDebugUtils), "Couldn't get 'VK_EXT_debug_utils' extension.");
+        VulkanException.ThrowsIf(!_vk.TryGetInstanceExtension(_instance, out _extDebugUtils), "Couldn't get 'VK_EXT_debug_utils' extension.");
 
-        var result = extDebugUtils!.CreateDebugUtilsMessenger(instance, GetDebugMessengerCreateInfo(), null, out debugMessenger);
+        var result = _extDebugUtils!.CreateDebugUtilsMessenger(_instance, GetDebugMessengerCreateInfo(), null, out _debugMessenger);
         VulkanException.ThrowsIf(result != Result.Success, $"Couldn't set up debug messenger: {result}.");
     }
 #endif
 
-    private unsafe void DestroyInstance() => vk.DestroyInstance(instance, null);
+    private unsafe QueueFamilyIndices FindQueueFamilies(PhysicalDevice physicalDevice)
+    {
+        QueueFamilyIndices queueFamilyIndices = new();
+
+        uint queueFamilyCount = 0;
+        _vk.GetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, null);
+
+        var queueFamilies = new QueueFamilyProperties[queueFamilyCount];
+        fixed (QueueFamilyProperties* queueFamiliesPtr = queueFamilies)
+        {
+            _vk.GetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamiliesPtr);
+        }
+
+        for (int i = 0; i < queueFamilyCount && !queueFamilyIndices.IsComplete(); i++)
+        {
+            if (queueFamilies[i].QueueFlags.HasFlag(QueueFlags.GraphicsBit))
+            {
+                queueFamilyIndices.Graphics = i;
+            }
+        }
+
+        return queueFamilyIndices;
+    }
+
+    private unsafe bool IsDeviceSuitable(PhysicalDevice physicalDevice, ref ulong memorySize)
+    {
+        var memoryProperties = _vk.GetPhysicalDeviceMemoryProperties(physicalDevice);
+        foreach (var heap in memoryProperties.MemoryHeaps.AsSpan())
+        {
+            if (heap.Flags.HasFlag(MemoryHeapFlags.DeviceLocalBit))
+            {
+                memorySize += heap.Size;
+            }
+        }
+
+        var queueFamiliyIndices = FindQueueFamilies(physicalDevice);
+        return queueFamiliyIndices.IsComplete();
+    }
+
+    private unsafe void PickPhysicalDevice()
+    {
+        uint deviceCount = 0;
+        var result = _vk.EnumeratePhysicalDevices(_instance, &deviceCount, null);
+        VulkanException.ThrowsIf(result != Result.Success, $"Couldn't enumerate physical devices: {result}.");
+        VulkanException.ThrowsIf(deviceCount == 0, "Couldn't find any GPUs with Vulkan API support.");
+
+        var physicalDevices = new PhysicalDevice[deviceCount];
+        fixed (PhysicalDevice* physicalDevicesPtr = physicalDevices)
+        {
+            result = _vk.EnumeratePhysicalDevices(_instance, &deviceCount, physicalDevicesPtr);
+            VulkanException.ThrowsIf(result != Result.Success, $"Couldn't get physical devices: {result}.");
+        }
+
+        // Picking GPU with most memory
+        // Cause most of the time the more memory GPU has the more performant it is
+        // Ideally all GPU parameters have to be taken into account
+        ulong maxMemorySize = 0;
+        for (int i = 0; i < physicalDevices.Length; i++)
+        {
+            ulong memorySize = 0;
+            if (IsDeviceSuitable(physicalDevices[i], ref memorySize) && memorySize > maxMemorySize)
+            {
+                maxMemorySize = memorySize;
+                _physicalDevice = physicalDevices[i];
+            }
+        }
+
+        VulkanException.ThrowsIf(_physicalDevice.Handle == 0, "Couldn't find suitable GPU.");
+
+        var deviceProperties = _vk.GetPhysicalDeviceProperties(_physicalDevice);
+        Console.WriteLine($"DeviceName: {SilkMarshal.PtrToString((nint)deviceProperties.DeviceName)}\n" +
+            $"\tApiVersion: {deviceProperties.ApiVersion}\n" +
+            $"\tDeviceID: {deviceProperties.DeviceID}\n" +
+            $"\tDeviceType: {deviceProperties.DeviceType}\n" +
+            $"\tDriverVersion: {deviceProperties.DriverVersion}\n" +
+            $"\tVendorID: {deviceProperties.VendorID}\n" +
+            $"\tMemorySize: {(double)maxMemorySize / 1024 / 1024 / 1024:F3} Gib\n");
+    }
+
+    private unsafe void DestroyInstance() => _vk.DestroyInstance(_instance, null);
 }
